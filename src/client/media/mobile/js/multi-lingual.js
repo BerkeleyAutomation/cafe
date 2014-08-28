d835a69be832757e0285b9c66a19243f0d2aa0e8
Amit <amitvakula@berkeley.edu>
2014-08-28 07:46:44 -0700

    fixed go-back id to work for spanish, updated translations, reposition buttons for cleanliness, and worked on getting multi-lingual support for help pages

--- src/client/media/mobile/js/multi-lingual.js
+++ src/client/media/mobile/js/multi-lingual.js
@@ -16,22 +16,21 @@
 			if (window.spanish=='spanish') {
 				$('.english').show();
 				$('.spanish').hide();
+				$('.instructions').html('Click any mug to begin.');
+				$('.instructions2').html('We want to hear your idea. Please rate one more suggestion first.');
+				$('.instructions3').html('Click your mug (in green) to share your suggestion.');
 				window.spanish = 'english';				
 			} else {
 				$('.english').hide();
 				$('.spanish').show();
+				$('.instructions').html('Haga clic en cualquier taza para comenzar.');
+				$('.instructions2').html('Queremos escuchar tu idea pero primero, por favor, califique una sugerencia más.');
+				$('.instructions3').html('Haga clic en su taza (en verde) para compartir su sugerencia.');
 				window.spanish = 'spanish';				
 			}	
 		}
 	);
 
 
-	$('#discuss-home-btn').click
-	(
-		function ()
-		{
-        	$('.landing').hide();
-        	$('#register').show();
-		}
-	);
+
 });
